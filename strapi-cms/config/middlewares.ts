2d64603cb12524f84e68e7c2a323bc004f78007f
nanameki1213 <nanamekitaishin1213@icloud.com>
2025-09-03 16:02:58 +0900

    [fix] strapiのcors設定をカスタム

--- strapi-cms/config/middlewares.ts
+++ strapi-cms/config/middlewares.ts
@@ -2,11 +2,20 @@ export default [
   'strapi::logger',
   'strapi::errors',
   'strapi::security',
-  'strapi::cors',
   'strapi::poweredBy',
   'strapi::query',
   'strapi::body',
   'strapi::session',
   'strapi::favicon',
   'strapi::public',
+
+  {
+    name: 'strapi::cors',
+    config: {
+      origin: ['*'],
+      methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'HEAD', 'OPTIONS'],
+      headers: ['Content-Type', 'Authorization', 'Origin', 'Accept'],
+      keepHeaderOnError: true,
+    },
+  },
 ];
