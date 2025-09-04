8480d5765c0013aaf4c4394a919b7c5adf88dda5
nanameki1213 <nanamekitaishin1213@icloud.com>
2025-09-05 00:59:45 +0900

    [change] apiを公開

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -3,10 +3,11 @@ import { Post } from "@/app/types/posts";
 
 // Read-Onlyのtoken
 const TOKEN = "f7ea886366caf77ef0acfc83d403024543f4726e8c8db2daf31138116e0c3df7142b9c0bf6882f089fd9c50c7837634d01ee55f82590329c6e44e81aef419530fc5ffafa9dd50e1f3b7aee85b0159b89e8784edadfc69a9f96dc80521e625f42a626470affa360aeabbae68871b0cc2ce45170020373dbe8e978739df6efc2a2"
+const URL = "http://shinysheep.net:1337";
 
 async function getPosts() {
   try {
-    const res = await fetch('http://localhost:1337/api/posts', {
+    const res = await fetch(URL + '/api/posts', {
       headers: {
         'Authorization': 'Bearer ' + TOKEN
       },
--- frontend/src/app/posts/[slug]/page.tsx
+++ frontend/src/app/posts/[slug]/page.tsx
@@ -1,6 +1,7 @@
-// app/articles/[slug]/page.tsx
+const URL = "http://shinysheep.net:1337";
+
 async function getPost(slug: string) {
-  const res = await fetch(`http://localhost:1337/api/posts?filters[Slug][$eq]=${slug}`, {
+  const res = await fetch(URL + `/api/posts?filters[Slug][$eq]=${slug}`, {
     headers: {
         'Authorization': 'Bearer f7ea886366caf77ef0acfc83d403024543f4726e8c8db2daf31138116e0c3df7142b9c0bf6882f089fd9c50c7837634d01ee55f82590329c6e44e81aef419530fc5ffafa9dd50e1f3b7aee85b0159b89e8784edadfc69a9f96dc80521e625f42a626470affa360aeabbae68871b0cc2ce45170020373dbe8e978739df6efc2a2'
       },
