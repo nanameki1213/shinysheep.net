2b51a0ab4d2736fa4712476d207294000bc1c13b
nanameki1213 <nanamekitaishin1213@icloud.com>
2025-09-05 03:42:18 +0900

    [change] TOKENを更新

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -2,18 +2,20 @@ import Link from "next/link";
 import { Post } from "@/app/types/posts";
 
 // Read-Onlyのtoken
-const TOKEN = "f7ea886366caf77ef0acfc83d403024543f4726e8c8db2daf31138116e0c3df7142b9c0bf6882f089fd9c50c7837634d01ee55f82590329c6e44e81aef419530fc5ffafa9dd50e1f3b7aee85b0159b89e8784edadfc69a9f96dc80521e625f42a626470affa360aeabbae68871b0cc2ce45170020373dbe8e978739df6efc2a2"
-const URL = "http://shinysheep.net:1337";
+const TOKEN = "9caa4699cb303422c9c1369fcb39df2abfef737aa5215f4684e592d9a0464ab037f2d2a7925dc8533506c904a2c7085d4354281a5864f19a0e1ada6c4e620cc7adbd157e8b938ec6a154e8d39ee160d6820a48ea38736c808fa1a4535a3acfc5695f578979285067a13fc2f84faff233ac6cee7543b53192acd5782f8eaecde8"
+const URL = "http://strapi:1337";
 
 async function getPosts() {
   try {
     const res = await fetch(URL + '/api/posts', {
+      cache: 'no-store',
       headers: {
         'Authorization': 'Bearer ' + TOKEN
       },
     });
     const data = await res.json();
     const posts: Post[] = data.data;
+    console.log(posts)
     return posts;
   } catch(err) {
     console.error('エラーが発生しました：', err);
@@ -21,6 +23,7 @@ async function getPosts() {
 }
 
 export default async function Home() {
+  console.log('Hello World!')
   const posts = await getPosts();
   if (posts == undefined) {
     console.error('記事一覧の取得に失敗しました。')
--- frontend/src/app/posts/[slug]/page.tsx
+++ frontend/src/app/posts/[slug]/page.tsx
@@ -1,9 +1,10 @@
-const URL = "http://shinysheep.net:1337";
+const TOKEN = "9caa4699cb303422c9c1369fcb39df2abfef737aa5215f4684e592d9a0464ab037f2d2a7925dc8533506c904a2c7085d4354281a5864f19a0e1ada6c4e620cc7adbd157e8b938ec6a154e8d39ee160d6820a48ea38736c808fa1a4535a3acfc5695f578979285067a13fc2f84faff233ac6cee7543b53192acd5782f8eaecde8"
+const URL = "http://strapi:1337";
 
 async function getPost(slug: string) {
   const res = await fetch(URL + `/api/posts?filters[Slug][$eq]=${slug}`, {
     headers: {
-        'Authorization': 'Bearer f7ea886366caf77ef0acfc83d403024543f4726e8c8db2daf31138116e0c3df7142b9c0bf6882f089fd9c50c7837634d01ee55f82590329c6e44e81aef419530fc5ffafa9dd50e1f3b7aee85b0159b89e8784edadfc69a9f96dc80521e625f42a626470affa360aeabbae68871b0cc2ce45170020373dbe8e978739df6efc2a2'
+        'Authorization': 'Bearer ' + TOKEN
       },
   });
   const data = await res.json();
