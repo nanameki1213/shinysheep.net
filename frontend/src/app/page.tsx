9c1060bfd967a2b54ad2c3d457fa8b830522f18e
nanameki1213 <nanamekitaishin1213@icloud.com>
2025-09-05 11:12:21 +0900

    [add] Radix UIを使ってUIをちょっとイイ感じにした

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,9 +1,10 @@
 import Link from "next/link";
 import { Post } from "@/app/types/posts";
+import { Heading, Flex, Card, Text, Separator } from '@radix-ui/themes'
 
 // Read-Onlyのtoken
 const TOKEN = "9caa4699cb303422c9c1369fcb39df2abfef737aa5215f4684e592d9a0464ab037f2d2a7925dc8533506c904a2c7085d4354281a5864f19a0e1ada6c4e620cc7adbd157e8b938ec6a154e8d39ee160d6820a48ea38736c808fa1a4535a3acfc5695f578979285067a13fc2f84faff233ac6cee7543b53192acd5782f8eaecde8"
-const URL = "http://strapi:1337";
+const URL = "http://100.86.44.46:1337";
 
 async function getPosts() {
   try {
@@ -14,6 +15,7 @@ async function getPosts() {
       },
     });
     const data = await res.json();
+    console.log(data)
     const posts: Post[] = data.data;
     console.log(posts)
     return posts;
@@ -27,18 +29,28 @@ export default async function Home() {
   const posts = await getPosts();
   if (posts == undefined) {
     console.error('記事一覧の取得に失敗しました。')
-    return;
+    return (
+      <Flex direction="column" gap="4">
+        <Heading size="6">記事一覧</Heading>
+        <Text color="red">記事の取得に失敗しました。</Text>
+      </Flex>
+    );
   }
   return (
-    <div>
-      <h1>記事一覧</h1>
-      <ul>
+    <Flex direction="column" gap="4" p="4">
+      <Heading size="6">最新の記事</Heading>
+      <Separator size="4" />
+      <Flex direction="column" gap="3">
         {posts.map((post: Post) => (
-          <li key={post.id}>
-            <Link href={`/posts/${post.Slug}`}>{post.Title}</Link>
-          </li>
+          <Link href={`/posts/${post.Slug}`} passHref key={post.id}>
+            <Card asChild>
+              <Flex direction="column" gap="2">
+                <Text as="p" size="5" weight="bold">{post.Title}</Text>
+              </Flex>
+            </Card>
+          </Link>
         ))}
-      </ul>
-    </div>
+      </Flex>
+    </Flex>
   );
 }
