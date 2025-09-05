f64ca270712ac79ad5de235a50766be6f18240ee
nanameki1213 <nanamekitaishin1213@icloud.com>
2025-09-05 11:31:43 +0900

    [change] APIのurlやトークンを.envで管理

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -3,8 +3,8 @@ import { Post } from "@/app/types/posts";
 import { Heading, Flex, Card, Text, Separator } from '@radix-ui/themes'
 
 // Read-Onlyのtoken
-const TOKEN = "9caa4699cb303422c9c1369fcb39df2abfef737aa5215f4684e592d9a0464ab037f2d2a7925dc8533506c904a2c7085d4354281a5864f19a0e1ada6c4e620cc7adbd157e8b938ec6a154e8d39ee160d6820a48ea38736c808fa1a4535a3acfc5695f578979285067a13fc2f84faff233ac6cee7543b53192acd5782f8eaecde8"
-const URL = "http://100.86.44.46:1337";
+const TOKEN = process.env.TOKEN
+const URL = process.env.API_URL
 
 async function getPosts() {
   try {
