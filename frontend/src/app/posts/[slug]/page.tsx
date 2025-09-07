88b4e2f63ae838d6c3fe566ac1c52adca1a36400
nanameki1213 <nanamekitaishin1213@icloud.com>
2025-09-07 15:03:37 +0900

    [style] yarn formatを実行

--- frontend/src/app/posts/[slug]/page.tsx
+++ frontend/src/app/posts/[slug]/page.tsx
@@ -1,33 +1,33 @@
-import { Heading, Flex, Text } from "@radix-ui/themes";
-import Post from "@/app/types/posts";
+import { Heading, Flex, Text } from '@radix-ui/themes'
+import Post from '@/app/types/posts'
 import ReactMarkdown from 'react-markdown'
 import Head from 'next/head'
 
 const TOKEN = process.env.TOKEN
-const URL = process.env.API_URL 
+const URL = process.env.API_URL
 
 async function getPost(slug: string) {
   try {
     const res = await fetch(URL + `/api/posts?filters[Slug][$eq]=${slug}&populate=*`, {
       headers: {
-          'Authorization': 'Bearer ' + TOKEN
-        },
-    });
-    const data = await res.json();
+        Authorization: 'Bearer ' + TOKEN,
+      },
+    })
+    const data = await res.json()
     if (data.data == null) {
       console.error('APIのエラーが発生しました：', data.error.detail)
     } else {
       const post: Post = data.data[0]
       return post
     }
-  } catch(err) {
+  } catch (err) {
     console.error('エラーが発生しました：', err)
   }
 }
 
 export default async function postPage({ params }: { params: { slug: string } }) {
   const pars = await params
-  const post = await getPost(pars.slug);
+  const post = await getPost(pars.slug)
   if (post == undefined) {
     console.error('記事一覧の取得に失敗しました。')
     return (
@@ -35,7 +35,7 @@ export default async function postPage({ params }: { params: { slug: string } })
         <Heading size="6">記事一覧</Heading>
         <Text color="red">記事の取得に失敗しました。</Text>
       </Flex>
-    );
+    )
   }
   return (
     <div>
@@ -44,10 +44,14 @@ export default async function postPage({ params }: { params: { slug: string } })
       </Head>
       <h1>{post.Title}</h1>
       <Flex gap="2">
-        <Text as="p" size="2" weight="light">{post.Published_Date}</Text>
-        <Text as="p" size="2">カテゴリ：{post.category.Name}</Text>
+        <Text as="p" size="2" weight="light">
+          {post.Published_Date}
+        </Text>
+        <Text as="p" size="2">
+          カテゴリ：{post.category.Name}
+        </Text>
       </Flex>
       <ReactMarkdown>{post.Content}</ReactMarkdown>
     </div>
-  );
+  )
 }
