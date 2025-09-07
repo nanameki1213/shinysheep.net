b42ae0194dc64963c42c99e6b2be0f6b7e45b4f7
nanameki1213 <nanamekitaishin1213@icloud.com>
2025-09-07 15:04:09 +0900

    [fix] eslintの設定を変更

--- frontend/eslint.config.mjs
+++ frontend/eslint.config.mjs
@@ -1,25 +1,77 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+import tseslint from '@typescript-eslint/eslint-plugin';
+import eslint from '@eslint/js';
+import eslintConfigPrettier from 'eslint-config-prettier';
+import importPlugin from 'eslint-plugin-import';
+import unusedImports from 'eslint-plugin-unused-imports';
+import pluginReactConfig from 'eslint-plugin-react/configs/recommended.js';
+import tsParser from '@typescript-eslint/parser';
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-});
-
-const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+export default  [
   {
-    ignores: [
-      "node_modules/**",
-      ".next/**",
-      "out/**",
-      "build/**",
-      "next-env.d.ts",
-    ],
+    files: ['**/*.ts', '**/*.tsx'], // 読み込むファイル
   },
+  {
+    ignores: ['**/.next/**/*'], // 無視するファイル
+  },
+  eslint.configs.recommended,
+  pluginReactConfig,
+  {
+    "settings": {
+      "react": {
+        "createClass": "createReactClass", // Regex for Component Factory to use,
+                                           // default to "createReactClass"
+        "pragma": "React",  // Pragma to use, default to "React"
+        "fragment": "Fragment",  // Fragment to use (may be a property of <pragma>), default to "Fragment"
+        "version": "detect", // React version. "detect" automatically picks the version you have installed.
+                             // You can also use `16.0`, `16.3`, etc, if you want to override the detected value.
+                             // Defaults to the "defaultVersion" setting and warns if missing, and to "detect" in the future
+        "defaultVersion": "", // Default React version to use when the version you have installed cannot be detected.
+                              // If not provided, defaults to the latest React version.
+        "flowVersion": "0.53" // Flow version
+      },
+    },
+  },
+  {
+    languageOptions: {
+      parser: tsParser,
+      globals: {
+        React: "readonly",
+      },
+    },
+  },
+  {
+    // eslint-plugin-importに関する設定
+    plugins: {
+      import: importPlugin,
+      '@typescript-eslint': tseslint,
+    },
+    rules: {
+      'import/order': [
+        'error',
+        {
+          alphabetize: { order: 'asc'},
+        },
+      ],
+    },
+  },
+  {
+    // eslint-plugin-unused-importsに関する設定
+    plugins: {
+      'unused-imports': unusedImports,
+    },
+    rules: {
+      'unused-imports/no-unused-imports': 'error',
+    },
+  },
+  {
+    // その他設定
+    linterOptions: {
+      reportUnusedDisableDirectives: "error",
+    },
+    rules: {
+      "react/jsx-boolean-value": "error", // JSXの中でのbooleanの使用
+      "react/jsx-curly-brace-presence": "error", // JSXの中での余分な{}の使用
+    },
+  },
+  eslintConfigPrettier, // Prettierとの競合防止
 ];
-
-export default eslintConfig;
