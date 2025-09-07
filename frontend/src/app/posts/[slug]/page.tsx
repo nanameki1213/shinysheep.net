e74e506789d45bac89a7765ddc7f2f862aee4742
nanameki1213 <nanamekitaishin1213@icloud.com>
2025-09-07 10:45:04 +0900

    [add] posts/slug/page.tsxにエラーハンドリングを追加

--- frontend/src/app/posts/[slug]/page.tsx
+++ frontend/src/app/posts/[slug]/page.tsx
@@ -1,24 +1,44 @@
+import { Heading, Flex, Text } from "@radix-ui/themes";
+import { Post } from "@/app/types/posts";
+
 const TOKEN = process.env.TOKEN
 const URL = process.env.API_URL 
 
 async function getPost(slug: string) {
-  const res = await fetch(URL + `/api/posts?filters[Slug][$eq]=${slug}`, {
-    headers: {
-        'Authorization': 'Bearer ' + TOKEN
-      },
-  });
-  const data = await res.json();
-  console.log(data);
-  return data.data;
+  try {
+    const res = await fetch(URL + `/api/posts?filters[Slug][$eq]=${slug}`, {
+      headers: {
+          'Authorization': 'Bearer ' + TOKEN
+        },
+    });
+    const data = await res.json();
+    if (data.data == null) {
+      console.error('APIのエラーが発生しました：', data.error.detail)
+    } else {
+      const post: Post = data.data[0]
+      return post
+    }
+  } catch(err) {
+    console.error('エラーが発生しました：', err)
+  }
 }
 
 export default async function postPage({ params }: { params: { slug: string } }) {
   const post = await getPost(params.slug);
-  console.log(post);
+  if (post == undefined) {
+    console.error('記事一覧の取得に失敗しました。')
+    return (
+      <Flex direction="column" gap="4">
+        <Heading size="6">記事一覧</Heading>
+        <Text color="red">記事の取得に失敗しました。</Text>
+      </Flex>
+    );
+  }
   return (
     <div>
-      <h1>{post[0].Title}</h1>
-      <p>{post[0].Content}</p>
+      <h1>{post.Title}</h1>
+      <Text as="p" size="2" weight="light">{post.Published_Date}</Text>
+      <p>{post.Content}</p>
     </div>
   );
 }
