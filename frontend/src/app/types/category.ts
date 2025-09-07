463e388406728be77ceb2443cd21f78d39389b5b
nanameki1213 <nanamekitaishin1213@icloud.com>
2025-09-07 13:37:41 +0900

    [add] Category型を定義

---
+++ frontend/src/app/types/category.ts
@@ -0,0 +1,5 @@
+export default interface Category {
+  id: number;
+  Name: string;
+  Slug: string;
+}
--- frontend/src/app/types/posts.ts
+++ frontend/src/app/types/posts.ts
@@ -1,7 +1,10 @@
+import Category from './category'
+
 export interface Post {
   id: number;
   Title: string;
   Published_Date: string;
   Slug: string;
   Content: string
+  Category: Category
 }
