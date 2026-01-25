6dae8231bfbecd4c39d13067d5f126c3796000d6
nanameki1213 <nanamekitaishin1213@icloud.com>
2026-01-25 23:59:39 +0900

    feat: run able

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -5,6 +5,15 @@ const nextConfig: NextConfig = {
   images: {
     unoptimized: true,
   },
+  async rewrites() {
+    return [
+      {
+        // Strapiの画像(uploads)もこのプロキシを通るようにする
+        source: '/api-proxy/:path*',
+        destination: 'http://strapi:1337/:path*',
+      },
+    ];
+  },
 };
 
 export default nextConfig;
